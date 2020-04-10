3b2756b92b98695e13a7882e99a28b84a8d5d98b
chenxiaobo <chenxiaobo.xian@haier.com>
2020-04-10 08:50:10 +0800

    添加左补零的方法

--- src/mcfile/McTool.ts
+++ src/mcfile/McTool.ts
@@ -18,18 +18,18 @@ export default class McTool {
         }
         return false;
     }
-}
 
-export function zeroLeftPad(str, len) {
-    const padding: string = "00000000";
-    const count: number = len - str.length;
+    static zeroLeftPad(str: string, len: number) {
+        const padding: string = "00000000";
+        const count: number = len - str.length;
 
-    if (count <= 0) return str;
+        if (count <= 0) return str;
 
-    if (count <= padding.length) {
-        return padding.substring(0, count) + str;
+        if (count <= padding.length) {
+            return padding.substring(0, count) + str;
+        }
+
+        str = padding + str;
+        return McTool.zeroLeftPad(str, len);
     }
-
-    str = padding + str;
-    return leftpad(str, len);
 }
